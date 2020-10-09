c37f7788e3c9200ac605a47ea5e5baa3b3b2c8cf
EmperorOfTrees <EmperorOfTrees@users.noreply.github.com>
2020-10-09 11:20:31 +0200

    Java to Kotlin update

--- app/src/main/java/com/project/e_card/MainActivity.kt
+++ app/src/main/java/com/project/e_card/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.project.e_card;
+package com.project.e_card
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
