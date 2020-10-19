1c5ccbccd9b3744c97b9f3dae050e50c063a1c55
MatthewAAU <44397196+MatthewAAU@users.noreply.github.com>
2020-10-19 20:41:57 +0200

    Layout and basic functionalities update
    Changed the appBar layput,
    Implemented basic login input check,
    Implemented app menu options to change the theme and sign out

--- app/src/main/java/com/project/e_card/LoginScreen.kt
+++ app/src/main/java/com/project/e_card/LoginScreen.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import kotlin.reflect.KClass
 
 class LoginScreen : AppCompatActivity() {
@@ -12,10 +14,30 @@ class LoginScreen : AppCompatActivity() {
         setContentView(R.layout.activity_login_screen)
         
         val loginButton = findViewById<Button>(R.id.buttonLogin)
+        val employeeEmail = findViewById<EditText>(R.id.employeeEmail)
+        val employeeNumber = findViewById<EditText>(R.id.employeeNumberInput)
+        val employeePassword = findViewById<EditText>(R.id.employeePassword)
+
+
 
         loginButton.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+
+            when {
+                employeeEmail.text.toString().trim().isEmpty() -> {
+                    employeeEmail.error = "You must provide an email!"
+                    Toast.makeText(this, "Email is required!", Toast.LENGTH_SHORT).show()
+                }
+                employeeNumber.text.toString().trim().isEmpty() -> {
+                    employeeNumber.error = "You must provide employee number!"
+                    Toast.makeText(this, "Employee number is required!", Toast.LENGTH_SHORT).show()
+                }
+                employeePassword.text.toString().trim().isEmpty() -> {
+                    employeePassword.error = "You must provide a password!"
+                    Toast.makeText(this, "Password is required!", Toast.LENGTH_SHORT).show()
+                }
+                else -> startActivity(intent)
+            }
         }
     }
 
--- app/src/main/java/com/project/e_card/MainActivity.kt
+++ app/src/main/java/com/project/e_card/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.project.e_card
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // this if checks if the dark mode is on or off
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolBar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(findViewById<View>(R.id.my_toolbar) as Toolbar?)
+
+        name = findViewById(R.id.name)
+        lastName = findViewById(R.id.lastName)
+        employeeNumber = findViewById(R.id.employeeNumber)
+
+    }
+    /**
+     * Method used to set the options menu in the tool bar
+     * @param menu
+     * @return
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+
+    /**
+     * Method listening for user input regarding the options menu
+     * @param item
+     * @return
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.LightTheme -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                restartApp()
+            }
+            R.id.DarkTheme -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                restartApp()
+            }
+            R.id.SignOut -> {
+                val intent = Intent(this,LoginScreen::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Restart application to the current screen
+     */
+    private fun restartApp() {
+        val i = Intent(applicationContext, MainActivity::class.java)
+        startActivity(i)
+        finish()
+    }
+
+    /**
+     * This method allows the user to leave the application once the back button is clicked.
+     */
+    override fun onBackPressed() {
+        moveTaskToBack(true)
+    }
+
+    companion object {
+        @JvmField
+        var name: TextView? = null
+        var lastName: TextView? = null
+        var employeeNumber: TextView? = null
+        var toolBar: Toolbar? = null
     }
 }
